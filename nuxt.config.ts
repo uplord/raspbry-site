cd76e26bb60582b7089fcd8074896d302ec6be71
Michael <michael@uplord.co.uk>
2024-10-21 16:48:07 +0100

    add font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    buildAssetsDir: "/nuxt/",
+    head: {
+      title: 'raspbry',
+      link: [,
+        {
+          rel: 'icon',
+          href: '/favicon.png'
+        },
+        {
+          rel: 'preload',
+          href: '/assets/fonts/ppneuemontreal-medium.woff2',
+          as: 'font',
+          type: 'font/woff',
+          crossorigin: 'anonymous'
+        },
+        {
+          rel: 'preload',
+          href: '/assets/fonts/ppneuemontreal-bold.woff2',
+          as: 'font',
+          type: 'font/woff',
+          crossorigin: 'anonymous'
+        }
+      ],
+      htmlAttrs: {
+        lang: "en"
+      },
+    }
+  },
   compatibilityDate: '2024-04-03',
   css: ['~/assets/scss/globals.scss'],
   devtools: { enabled: true },
