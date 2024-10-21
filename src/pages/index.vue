1e1ea7a9ac1ecf6b2274e8360138c34437a30d9d
Michael <michael@uplord.co.uk>
2024-10-21 16:41:48 +0100

    logo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Raspbry</h1>
+  <section class="py-6 flex items-center justify-center h-screen">
+    <div class="container mx-auto px-4 max-w-full lg:max-w-screen-xl">
+      <div class="flex flex-col items-center justify-center gap-4">
+        <nuxt-img preload src="/images/logo.svg" alt="raspbry" width="80" />
+        <h1 class="text-center text-4xl font-bold">raspbry</h1>
+        <h2 class="text-center text-xl">website coming soon</h2>
+      </div>
+    </div>
+  </section>
 </template>
